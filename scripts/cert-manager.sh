d728b3bbb32f0ea35db4402e3802098561904d02
samxiang1974 <samxiang1974@gmail.com>
2020-03-16 19:08:16 +1100

    last update after testing

--- scripts/cert-manager.sh
+++ scripts/cert-manager.sh
@@ -3,11 +3,12 @@
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.0/cert-manager.crds.yaml
 kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
-helm repo update && helm install --name cert-manager --namespace cert-manager --set rbac.create=true jetstack/cert-manager \
+helm repo update && helm install --name cert-manager --namespace cert-manager --set global.rbac.create=true jetstack/cert-manager \
   --set ingressShim.defaultIssuerName=letsencrypt-prod \
-  --set ingressShim.defaultIssuerKind=ClusterIssuer
+  --set ingressShim.defaultIssuerKind=ClusterIssuer \
+  --set ingressShim.defaultIssuerGroup=cert-manager.io
 cat <<EOF | kubectl apply -f -
-apiVersion: certmanager.k8s.io/v1alpha2
+apiVersion: cert-manager.io/v1alpha3
 kind: ClusterIssuer
 metadata:
   name: letsencrypt-prod
@@ -21,5 +22,8 @@ spec:
     privateKeySecretRef:
       name: letsencrypt-prod
     # Enable the HTTP-01 challenge provider
-    http01: {}
+    solvers:
+    - http01: 
+        ingress:
+          class: nginx
 EOF
